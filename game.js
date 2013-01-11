de738ab7b3b2c3fb09b171ce679cc6897ed6ded5
Elizar Pepino <jupenz@gmail.com>
2013-01-11 16:06:13 +0800

    Fixed stack_overflow issue and added game turn-around logic

--- game.js
+++ game.js
@@ -2,37 +2,64 @@
 
 	// The Game Class
 	var TicTacToe = function() {
-		// Private prop
+		// Private
 		var _currentPlayer,
+			_players,
 			_setCurrentPlayer = function () {
-				_currentPlayer = (_currentPlayer === 0)? 1 : 0;
-			},
-			_computerSelect = function() {
-				// This is AI mode baby!
+
+				_currentPlayer = !_currentPlayer ? 1 : _currentPlayer === 1 ? 0 : 1;
+
+				var player = _players[_currentPlayer];
+				tictactoe.player = player;
+				console.log("%s's Turn:\n", player.name);
+				if (player.name === 'Computer') {
+					// pass the player computer as an argument
+					player.aiSelect();
+				} else {
+					console.log("Please select a block...\n");
+				}
+
 			},
 			_checkWin = function(player) {
 				if (player.isWin()) {
 					console.log(player.name + " WON!!! \nRestarting game...\n");
+					tictactoe.start();
+				}
+				else {
+					_setCurrentPlayer();
 				}
 			};
 
-		// Public prop
-		this.players = [];
+		// Public
+		
+		this.player = null;
+		this.board = [];
+
 		this.start = function() {
-			this.players.push(new Player("Player1"));
+
+			_players = [];
+			_currentPlayer = 0;
+			this.board = [
+				[0,0,0],
+				[0,0,0],
+				[0,0,0]
+			];
+
+			_players.push(new Player("Player1"));
 			console.log("Player1 added.\n");
-			this.players.push(new Player("Computer"));
+			_players.push(new Player("Computer"));
 			console.log("Computer added.\n");
 
 			// Add select listeners
-			for (var i = this.players.length - 1; i >= 0; i--) {
-				this.players[i].on('select', _checkWin);
+			for (var i = _players.length - 1; i >= 0; i--) {
+				_players[i].on('select', _checkWin);
 			}
 
-			// Randomly select first player to play.
-			_currentPlayer = Math.round(Math.random());
+			// Initialize player
+			_setCurrentPlayer();
 
-		};
+		},
+		tictactoe = this; // point tictactoe to this for private methods
 	};
 
 	// The player class
@@ -40,6 +67,9 @@
 		// Private prop
 		var events = {
 				'select' : {
+					cbs : []
+				},
+				'won' : {
 					cbs : []
 				}
 			},
@@ -65,66 +95,75 @@
 			events[type].cbs.push(cb);
 		};
 		this.select = function(x, y) {
-			if (this.board[x][y] !== 1) {
+			if (tictactoe.board[x][y] !== 1) {
+				tictactoe.board[x][y] = 1;
 				this.board[x][y] = 1;
-				emitEvent('select');
-			}
-		};
-		this.aiSelect = function(x, y) {
-			if (this.board[x][y] !== 1) {
-				this.board[x][y] = 1;
+				console.log("%s  selected [%d][%d] block", this.name, x, y);
 				emitEvent('select');
 			} else {
-				this.aiSelect(x, y);
+				console.log("Warning: Block Taken!\n Please select another block");
+			}
+		};
+		this.aiSelect = function() {
+
+			var x = _getRandomNumber(2),
+				y = _getRandomNumber(2);
+
+			if (tictactoe.board[x][y] !== 1) {
+				tictactoe.board[x][y] = 1;
+				this.board[x][y] = 1;
+				console.log("%s  selected [%d][%d] block", this.name, x, y);
+				emitEvent('select');
+			} else {
+				setTimeout(this.aiSelect(), 2000);
 			}
 		};
 		this.isWin = function() {
 			if (this.board[0][0] === 1 && this.board[0][1] === 1 && this.board[0][2] === 1) {
-				console.log(1);
 				return true;
 			}
 			if (this.board[1][0] === 1 && this.board[1][1] === 1 && this.board[1][2] === 1) {
-				console.log(2);
 				return true;
 			}
 			if (this.board[2][0] === 1 && this.board[2][1] === 1 && this.board[2][2] === 1) {
-				console.log(3);
 				return true;
 			}
 			if (this.board[0][0] === 1 && this.board[1][0] === 1 && this.board[2][0] === 1) {
-				console.log(4);
 				return true;
 			}
 			if (this.board[1][0] === 1 && this.board[1][1] === 1 && this.board[2][1] === 1) {
-				console.log(5);
 				return true;
 			}
 			if (this.board[0][2] === 1 && this.board[1][2] === 1 && this.board[2][2] === 1) {
-				console.log(6);
 				return true;
 			}
 			if (this.board[0][0] === 1 && this.board[1][1] === 1 && this.board[2][2] === 1) {
-				console.log(7);
 				return true;
 			}
 			if (this.board[0][2] === 1 && this.board[1][1] === 1 && this.board[2][0] === 1) {
-				console.log(8);
 				return true;
 			}
-			console.log("not win!");
 			return false;
 		};
 	};
-
-	function init() {
-		console.log("-!- Game is starting \n-!- Prepare yourself!");
-		w.tictactoe = new TicTacToe();
-	}
 
 	w.onload = function(e) {
 		if (document.readyState === "complete") {
 			init();
 		}
 	};
+
+	function init() {
+		console.log("-!- Game is starting \n-!- Prepare yourself!");
+		tictactoe = new TicTacToe();
+		tictactoe.start();
+	}
+
+	function _getRandomNumber(range) {
+		if (!range) {
+			return Math.round(Math.random());
+		}
+		return Math.round(Math.random() * range);
+	}
 
 })(window);
