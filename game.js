7f3181dacb58f447d08f118930aa5a7125ca70cc
Elizar Pepino <jupenz@gmail.com>
2013-01-13 03:29:04 +0800

    Refactored game logic, TODO: Add gui

--- game.js
+++ game.js
@@ -1,28 +1,44 @@
+/** Copyright (c) 2013 Elizar Pepino
+* @author Elizar Pepino
+* @website http://penzur.com | http://elizarpepino.com
+* @version 0.0.1
+* @description  This is a basic tic tac toe game based on javascript
+*               nothing fancy. I built this while I was trying to teach my cousin
+*               some basic javascript.
+*/
 (function(w) {
 
-    // The Game Class
+    /** The Game Class
+    * @params:
+    */
     var TicTacToe = function() {
-        // Private
+        /** Private variables
+        */
         var _currentPlayer,
+        _tictactoe = this,
         _players,
+
+        /** Fires after _checkWin() if player.isWin() returns false
+        * @property private
+        * @params:
+        */
         _setCurrentPlayer = function() {
             
+            tictactoe.player = _players[0];
             _currentPlayer = !_currentPlayer ? 1 : _currentPlayer === 1 ? 0 : 1;
-            
             var player = _players[_currentPlayer];
-            tictactoe.player = player;
-            console.log("%s's Turn:\n", player.name);
-            if (player.name === 'Computer') {
-                // pass the player computer as an argument
-                player.aiSelect();
-            } else {
-                console.log("Please select a block...\n");
-            }
+            player.select();
         
         },
+
+        /** Fires when a user emmits a "select" event then checks to see if player wins.
+        * @property private
+        * @params:
+        * <Player> player - An instance of a Player class
+        */
         _checkWin = function(player) {
             if (player.isWin()) {
-                console.log(player.name + " WON!!! \nRestarting game...\n");
+                console.log(player.getName() + " WON!!! Restarting game...");
                 tictactoe.start();
             }
             else {
@@ -30,13 +46,17 @@
             }
         };
 
-        // Public
-        
+        /** Public variables
+        */
         this.player = null;
         this.board = [];
         
+        /** Start/restart Game
+        * @property public
+        * @params:
+        */
         this.start = function() {
-            
+            // set/re-set everything
             _players = [];
             _currentPlayer = 0;
             this.board = [
@@ -44,103 +64,176 @@
                 [0, 0, 0],
                 [0, 0, 0]
             ];
+            console.log("-!- Game is starting\n-!- Prepare yourself!");
             
-            _players.push(new Player("Player1"));
-            console.log("Player1 added.\n");
-            _players.push(new Player("Computer"));
-            console.log("Computer added.\n");
+            // Add new players
+            _players.push(new Player({name: "Elizar", type: "player"}));
+            console.log("-!- Player Elizar joined the Game.");
+            _players.push(new Player({name: "Computer", type:"ai"}));
+            console.log("-!- Player Computer joined the game.");
 
-            // Add select listeners
+            // Add listeners to players' "select" event
             for (var i = _players.length - 1; i >= 0; i--) {
                 _players[i].on('select', _checkWin);
             }
 
-            // Initialize player
+            // Set the first player to select, defaults to Computer
             _setCurrentPlayer();
         
-        },
-        tictactoe = this; // point tictactoe to this for private methods
+        };
     };
 
-    // The player class
-    var Player = function(name) {
-        // Private prop
-        var events = {
-            'select': {
-                cbs: []
-            },
-            'won': {
-                cbs: []
-            }
-        },
-        emitEvent = function(type) {
-            var cbs = events[type].cbs;
-            for (var i = cbs.length - 1; i >= 0; i--) {
-                cbs[i](player);
-            }
-        },
-        // Set player variable equals to self/this
-        player = this;
+    /** The Player class
+    * @params:
+    * <Object> p - An object for setting player's attributes
+    *              Accepted datas are:
+    *              name - <String> defaults to "Player"
+    *              type - <String> accepted values are "ai" and "player", defaults to "ai"
+    */
+    var Player = function(p) {
 
-        // Player's name defaults to Player
-        this.name = name || "Player";
-        // Player's main board
-        this.board = [
+        /** Private variables
+        */
+        var _events = {
+            'select': {
+                callbacks: []
+            }
+        },
+        _p = p || {},
+        _type = _p.type || "ai",
+        _name = _p.name || "Player",
+        _player = this, // Will be use as an accessor to this prototype inside private methods.
+        _board = [
             [0, 0, 0],
             [0, 0, 0],
             [0, 0, 0]
-        ];
-        // Add event listeners
-        this.on = function(type, cb) {
-            events[type].cbs.push(cb);
-        };
-        this.select = function(x, y) {
-            if (tictactoe.board[x][y] !== 1) {
-                tictactoe.board[x][y] = 1;
-                this.board[x][y] = 1;
-                console.log("%s  selected [%d][%d] block", this.name, x, y);
-                emitEvent('select');
-            } else {
-                console.log("Warning: Block Taken!\n Please select another block");
+        ],
+        
+        /** Works as EventEmitter and passes the player that's emmiting the event to the call_back function
+        * @property private
+        * @params: <String> type - The type of event that will be emmited
+        */
+        _emitEvent = function(type) {
+            var delegates = _events[type].callbacks;
+            for (var i = delegates.length - 1; i >= 0; i--) {
+                delegates[i](_player);
             }
-        };
-        this.aiSelect = function() {
-            
+        },
+
+        /** Method that checks if block is free or already selected
+        * @property private
+        * @params:
+        * <Number> x - The horizontal position of the block
+        *          y - The vertical position of the block
+        * @returns:
+        * <Boolean> true - If block is free,
+        *           false - If block is already taken
+        */
+        _isBlockFree = function(x, y) {
+            if (tictactoe.board[x][y] !== 1) {
+                return true;
+            }
+            return false;
+        },
+
+        /** Method that activates a block on both the Game and Player's board
+        * @property private
+        * @params:
+        * <Number> x - The horizontal position of the block
+        *          y - The vertical position of the block
+        */
+        _activateBlock = function(x, y) {
+            tictactoe.board[x][y] = 1;
+            _board[x][y] = 1;
+            console.log("-!- %s  selected [%d][%d] block", _name, x, y);
+            _emitEvent('select');
+        },
+        
+        /** Method for selecting a block on the board for non-ai player only
+        * @property private
+        * @params:
+        * <Number> x - Horizontal position of the block
+        * <Number> y - The vertical position of the block
+        */
+        _playerSelect = function(x, y) {
+            if (_isBlockFree(x, y)) {
+                _activateBlock(x, y);
+            } else {
+                console.log("-!- Warning: Block Taken! Please select another block");
+            }
+        },
+
+        /** Method for selecting a block on the board for AI player only
+        * @property private
+        */
+        _aiSelect = function() {
             var x = _getRandomNumber(2),
-            y = _getRandomNumber(2);
+                y = _getRandomNumber(2);
             
-            if (tictactoe.board[x][y] !== 1) {
-                tictactoe.board[x][y] = 1;
-                this.board[x][y] = 1;
-                console.log("%s  selected [%d][%d] block", this.name, x, y);
-                emitEvent('select');
+            if (_isBlockFree(x, y)) {
+                _activateBlock(x, y);
             } else {
-                setTimeout(this.aiSelect(), 2000);
+                _aiSelect();
             }
         };
+
+
+        /** Tell player that it's his/her turn
+        * @property public
+        */
+        this.select = function() {
+
+            console.log("-!- It's %s's turn!", _name);
+            if (_type === "ai") {
+                console.log("-!- %s is selecting a block...", _name);
+                setTimeout(_aiSelect,2000);
+            }
+        };
+        /** A public interface to let the user select a block
+        * @property public
+        * @note This is a temporary implementation and will be removed when implementing GUI
+        */
+        this.selectBlock = function(x, y) {
+            _playerSelect(x, y);
+        };
+
+        /** Method for adding event listeners
+        * @property public
+        * @params:
+        * <String> type - Event type i.e: 'select'
+        * <Function> cb - A callback function
+        */
+        this.on = function(type, cb) {
+            _events[type].callbacks.push(cb);
+        };
+
+        this.getName = function() {
+            return _name;
+        };
+
         this.isWin = function() {
-            if (this.board[0][0] === 1 && this.board[0][1] === 1 && this.board[0][2] === 1) {
+            if (_board[0][0] === 1 && _board[0][1] === 1 && _board[0][2] === 1) {
                 return true;
             }
-            if (this.board[1][0] === 1 && this.board[1][1] === 1 && this.board[1][2] === 1) {
+            if (_board[1][0] === 1 && _board[1][1] === 1 && _board[1][2] === 1) {
                 return true;
             }
-            if (this.board[2][0] === 1 && this.board[2][1] === 1 && this.board[2][2] === 1) {
+            if (_board[2][0] === 1 && _board[2][1] === 1 && _board[2][2] === 1) {
                 return true;
             }
-            if (this.board[0][0] === 1 && this.board[1][0] === 1 && this.board[2][0] === 1) {
+            if (_board[0][0] === 1 && _board[1][0] === 1 && _board[2][0] === 1) {
                 return true;
             }
-            if (this.board[1][0] === 1 && this.board[1][1] === 1 && this.board[2][1] === 1) {
+            if (_board[1][0] === 1 && _board[1][1] === 1 && _board[2][1] === 1) {
                 return true;
             }
-            if (this.board[0][2] === 1 && this.board[1][2] === 1 && this.board[2][2] === 1) {
+            if (_board[0][2] === 1 && _board[1][2] === 1 && _board[2][2] === 1) {
                 return true;
             }
-            if (this.board[0][0] === 1 && this.board[1][1] === 1 && this.board[2][2] === 1) {
+            if (_board[0][0] === 1 && _board[1][1] === 1 && _board[2][2] === 1) {
                 return true;
             }
-            if (this.board[0][2] === 1 && this.board[1][1] === 1 && this.board[2][0] === 1) {
+            if (_board[0][2] === 1 && _board[1][1] === 1 && _board[2][0] === 1) {
                 return true;
             }
             return false;
@@ -154,7 +247,6 @@
     };
     
     function init() {
-        console.log("-!- Game is starting \n-!- Prepare yourself!");
         tictactoe = new TicTacToe();
         tictactoe.start();
     }
